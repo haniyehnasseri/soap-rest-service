75fa4089e2ccfe324f0f982079af5314d5cfd3de
Haniyeh Nasseri <haniyeh.nasseri99@gmail.com>
2020-10-01 23:24:45 +0330

    soap/rest

---
+++ src/main/java/example/hello.java
@@ -0,0 +1,16 @@
+package example;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class hello {
+
+    @WebMethod
+    public String bonjour(String name) {
+        return String.format("Bonjour %s", name);
+    }
+}
