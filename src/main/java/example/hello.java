2946eff2ae345f59799d1fbeed1b87dd67313c8f
Haniyeh Nasseri <haniyeh.nasseri99@gmail.com>
2020-10-06 12:03:40 +0330

    soap

--- src/main/java/example/hello.java
+++ src/main/java/example/hello.java
@@ -4,6 +4,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @WebService
 @SOAPBinding(style = Style.RPC)
@@ -11,6 +15,31 @@ public class hello {
 
     @WebMethod
     public String bonjour(String name) {
-        return String.format("Bonjour %s", name);
+        //return String.format("Bonjour %s", name);
+        String response = "";
+        try {
+
+            URL url = new URL("http://localhost:8081/user?name="+name);//your url i.e fetch data from .
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            //conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : "
+                        + conn.getResponseCode());
+            }
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            String output;
+            while ((output = br.readLine()) != null) {
+                //System.out.println(output);
+                response = response + output;
+                response = response + "\n";
+            }
+            conn.disconnect();
+
+        } catch (Exception e) {
+            System.out.println("Exception in NetClientGet:- " + e);
+        }
+        return response;
     }
 }
